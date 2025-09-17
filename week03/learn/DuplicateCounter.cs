3aa7d1ed5e3962f1e62257c91952691d09ac602f
Abiloye <iabiloye@gmail.com>
2025-09-17 18:22:56 +0100

    Complex Map Data

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -23,8 +23,9 @@
     }
 
     private static int CountDuplicates(int[] data)
+    
     {
-        // Add code here.
-        return 0;
+        var unique = new HashSet<int>(data);
+        return data.Length - unique.Count;
     }
 }
--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -1,3 +1,5 @@
+//Abiloye
+
 public class Translator
 {
     public static void Run()
@@ -24,7 +26,7 @@ public class Translator
     /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words[fromWord] = toWord;
     }
 
     /// <summary>
@@ -32,9 +34,16 @@ public class Translator
     /// </summary>
     /// <param name="fromWord">The word to translate</param>
     /// <returns>The translated word or "???" if no translation is available</returns>
+
     public string Translate(string fromWord)
     {
-        // ADD YOUR CODE HERE
-        return "";
+        string newWord = "???";
+
+        if (_words.ContainsKey(fromWord))
+        {
+            newWord = _words[fromWord];
+        }
+
+        return newWord;
     }
 }
